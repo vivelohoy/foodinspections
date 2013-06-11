76289dee67170bc987bf24acfd1d7aa198eabe33
wilbertom <wilbertomorales777@gmail.com>
2013-06-11 10:40:30 -0500

    Add server database settings

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 #DEBUG = True
 #SQLALCHEMY_ECHO=True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'inspections.db')
+if 'RDS_HOSTNAME' in os.environ:
+    SQLALCHEMY_DATABASE_URI = 'mysql://' + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] +'@' + os.environ['RDS_HOSTNAME']  + '/' + os.environ['RDS_DB_NAME']
+   
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'inspections.db')
 
 THREADS_PER_PAGE = 8
