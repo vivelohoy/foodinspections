1bbbbf2cb2b84c0da99680640c2adedaa58fa7fe
wilbertom <wilbertomorales777@gmail.com>
2013-06-19 14:58:53 -0500

    Facility view and blueprint, macros implemented

---
+++ inspections/facility/facilityblueprint.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase
+from flask import Blueprint, render_template, redirect, url_for, Response
+from inspections.models.models import *
+
+facility_blueprint = Blueprint("facility_blueprint", __name__)
+
+@facility_blueprint.route("/facility/<facility_name>/<address>/")
+def show_facility(facility_name, address):
+
+    print facility_name, address + "<- Address"
+
+    data = Facilities.query.get((str(facility_name),str(address)))
+    print data
+    return render_template("facility/single-facility.html", facility=data )
