40c5ba78567bbe6d7a1bd41877766554aeac7840
wilbertom <wilbertomorales777@gmail.com>
2013-07-08 14:36:54 -0500

    GET method search for bookmarkable results and google analytics

--- inspections/search/searchblueprint.py
+++ inspections/search/searchblueprint.py
@@ -30,17 +30,16 @@ def search_db(search_keyword, original_search):
 
 
 
-@search_blueprint.route("/search", methods=['POST', 'GET'])
+@search_blueprint.route("/search")
 def search():
-    if request.method == 'POST':
-        if request.form['main-data']:
-            original_search = request.form['main-data']
+    if request.method == 'GET':
+        if 'q' in request.args:
+            original_search = request.args['q']
             length_of_search = len(original_search)
             application.logger.info('Searched for: %s at %s' % (original_search, datetime.datetime.today()))
             if length_of_search <= 3: # Query too big to perform on sane time
                 return render_template('search/no-search.html', response_message="Search term too short")
-            search_keyword = request.form['main-data'].replace('\'', '').replace('"', '').replace('<', '').replace('>', '')
-
+            search_keyword = request.args['q'].replace('\'', '').replace('"', '').replace('<', '').replace('>', '')
             return search_db(search_keyword, original_search)
 
         else: 
